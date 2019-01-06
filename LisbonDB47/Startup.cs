b1d0dad9323da8a194b0ac751b63a119162f5603
Tomek Buslowski <tbuslowski@gmail.com>
2019-01-06 18:43:03 +0000

    Allow CORS

--- LisbonDB47/Startup.cs
+++ LisbonDB47/Startup.cs
@@ -23,6 +23,7 @@ namespace LisbonDB47
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
         }
 
         public IConfiguration Configuration { get; }
@@ -30,21 +31,24 @@ namespace LisbonDB47
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             //var connection = @"Server=(localdb)\mssqllocaldb;Database=LisbonDB47;Trusted_Connection=True;ConnectRetryCount=0";
             // services.AddDbContext<LisbonDbContext>(options => options.UseSqlServer(connection));
             var connection = "User ID=slightom;Password=carpediem1020;Host=localhost;Port=5432;Database=lisbondb47;Pooling=true;";
             services.AddDbContext<LisbonDbContext>(options => options.UseNpgsql(connection));
 
-            services.Configure<ForwardedHeadersOptions>(options =>
-            {
-                options.KnownProxies.Add(IPAddress.Parse("212.237.17.69"));
-            });
+            //services.Configure<ForwardedHeadersOptions>(options =>
+            //{
+            //    options.KnownProxies.Add(IPAddress.Parse("212.237.17.69"));
+            //});
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            services.AddMvc().AddJsonOptions(options => {
+            services.AddMvc().AddJsonOptions(options =>
+            {
                 options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
             });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -63,6 +67,15 @@ namespace LisbonDB47
             {
                 app.UseHsts();
             }
+
+            app.UseCors(builder =>
+            {
+                builder.AllowAnyHeader();
+                builder.AllowAnyMethod();
+                builder.AllowCredentials();
+                builder.AllowAnyOrigin();
+            });
+
 
             app.UseHttpsRedirection();
             app.UseMvc();
